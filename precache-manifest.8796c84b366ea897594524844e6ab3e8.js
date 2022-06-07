9da9987d5a250a95500a74270d8a457a8cd9053d
Ketan Pawar <ketanpawar@FVFGV626Q05N.local>
2022-06-07 11:09:22 +0530

    Updates

---
+++ precache-manifest.8796c84b366ea897594524844e6ab3e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a9ce9e9f3f3ea37eff963fb0ac33c1",
+    "url": "/learningReact_groupedBar-mockdata/index.html"
+  },
+  {
+    "revision": "070ff33022efa3849c31",
+    "url": "/learningReact_groupedBar-mockdata/static/css/main.3a0eac0d.chunk.css"
+  },
+  {
+    "revision": "c14f4d357580abd05f4e",
+    "url": "/learningReact_groupedBar-mockdata/static/js/2.35f145d3.chunk.js"
+  },
+  {
+    "revision": "da11baabf64dfea4c5dd064cac47732e",
+    "url": "/learningReact_groupedBar-mockdata/static/js/2.35f145d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "070ff33022efa3849c31",
+    "url": "/learningReact_groupedBar-mockdata/static/js/main.ce596847.chunk.js"
+  },
+  {
+    "revision": "bd4b1973826a32c31a31",
+    "url": "/learningReact_groupedBar-mockdata/static/js/runtime-main.5be0795b.js"
+  }
+]);
